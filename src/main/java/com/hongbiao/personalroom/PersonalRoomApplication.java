6895a9d9f096e5d3572333d51df60a7445388bf1
flipped8 <2927875758@qq.com>
2024-09-10 23:21:08 +0800

    commit

---
+++ src/main/java/com/hongbiao/personalroom/PersonalRoomApplication.java
@@ -0,0 +1,13 @@
+package com.hongbiao.personalroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalRoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hongbiao/personalroom/PersonalRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongbiao.personalroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
